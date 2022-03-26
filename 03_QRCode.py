cf876b189b563c0c3d683fe426f1c2b60925a482
shashichawan <102402158+shashichawan@users.noreply.github.com>
2022-03-26 10:40:45 +0530

    Add files via upload

---
+++ 03_QRCode.py
@@ -0,0 +1,7 @@
+import pyqrcode 
+from pyqrcode import QRCode
+import png
+s = "https://joshinnovations.com/"
+url = pyqrcode.create(s) 
+# url.svg('josh_QR_Code.svg',scale=2)
+url.show("josh.png", scale = 2) 
